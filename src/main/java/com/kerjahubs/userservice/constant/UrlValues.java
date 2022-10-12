46d1f6c6c05ef371f9280b3ebe38843a757e5821
michaelloi <loimichael0@gmail.com>
2022-10-12 20:07:05 +0700

    prod

--- src/main/java/com/kerjahubs/userservice/constant/UrlValues.java
+++ src/main/java/com/kerjahubs/userservice/constant/UrlValues.java
@@ -1,15 +1,15 @@
 package com.kerjahubs.userservice.constant;
 
 public class UrlValues {
-    public static final String login = "user/login";
-    public static final String inquiryProfilePartner = "user/inquiry-profile-partner";
-    public static final String registerRetail = "user/register-retail";
-    public static final String editProfileRetail = "user/edit-profile-data-retail";
-    public static final String registerPartner = "user/register-partner";
-    public static final String kelas = "user/add-product";
-    public static final String modul = "user/add-module-product";
-    public static final String benefit = "user/add-benefit-product";
-    public static final String changePassword = "user/change-password";
-    public static final String getBanners = "config/banners";
-    public static final String getParameters = "config/parameters";
+    public static final String login = "login";
+    public static final String inquiryProfilePartner = "inquiry-profile-partner";
+    public static final String registerRetail = "register-retail";
+    public static final String editProfileRetail = "edit-profile-data-retail";
+    public static final String registerPartner = "register-partner";
+    public static final String kelas = "add-product";
+    public static final String modul = "add-module-product";
+    public static final String benefit = "add-benefit-product";
+    public static final String changePassword = "change-password";
+    public static final String getBanners = "banners";
+    public static final String getParameters = "parameters";
 }
